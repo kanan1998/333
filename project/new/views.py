e5b8cc1de30fc8f8ce1de9c0bc865ebdae64d5fe
kanan1998 <124672901+kanan1998@users.noreply.github.com>
2023-05-05 22:19:07 +0700

    Add files via upload

---
+++ project/new/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'new_portal/index.html')
+
+
+def about(request):
+    return render(request, 'new_portal/about.html')
